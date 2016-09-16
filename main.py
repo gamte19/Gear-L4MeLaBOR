130634bb701a48339b2d8926448e37f346831646
Milu László <hockeylaci@gmail.com>
2016-09-16 16:35:30 +0200

    skeleton of the game is half-ready

--- main.py
+++ main.py
@@ -1,1 +1,99 @@
+from pong import *
 import pygame
+
+def main():
+    """ Main Program """
+
+    # Call this function so the Pygame library can initialize itself
+    pygame.init()
+
+    # Create an 800x600 sized screen
+    screen = pygame.display.set_mode([800, 600])
+
+    # Set the title of the window
+    pygame.display.set_caption('Pong')
+
+    # Create the player paddle object
+    player = Player(150, 290)
+    player2 = Player(550, 290)
+    movingsprites = pygame.sprite.Group()
+    movingsprites.add(player)
+    movingsprites.add(player2)
+
+    rooms = []
+
+    room = Room()
+    rooms.append(room)
+
+    current_room_no = 0
+    current_room = rooms[current_room_no]
+
+    clock = pygame.time.Clock()
+
+    done = False
+
+    while not done:
+
+        # --- Event Processing ---
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.changespeed(-5, 0)
+                if event.key == pygame.K_RIGHT:
+                    player.changespeed(5, 0)
+                if event.key == pygame.K_UP:
+                    player.changespeed(0, -5)
+                if event.key == pygame.K_DOWN:
+                    player.changespeed(0, 5)
+                # player2
+                if event.key == pygame.K_a:
+                    player2.changespeed(-5, 0)
+                if event.key == pygame.K_d:
+                    player2.changespeed(5, 0)
+                if event.key == pygame.K_w:
+                    player2.changespeed(0, -5)
+                if event.key == pygame.K_s:
+                    player2.changespeed(0, 5)
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player.changespeed(5, 0)
+                if event.key == pygame.K_RIGHT:
+                    player.changespeed(-5, 0)
+                if event.key == pygame.K_UP:
+                    player.changespeed(0, 5)
+                if event.key == pygame.K_DOWN:
+                    player.changespeed(0, -5)
+                # player2
+                if event.key == pygame.K_a:
+                    player2.changespeed(5, 0)
+                if event.key == pygame.K_d:
+                    player2.changespeed(-5, 0)
+                if event.key == pygame.K_w:
+                    player2.changespeed(0, 5)
+                if event.key == pygame.K_s:
+                    player2.changespeed(0, -5)
+
+        # --- Game Logic ---
+
+        player.move(current_room.wall_list)
+        player2.move(current_room.wall_list)
+
+        # --- Drawing ---
+        screen.fill(BLACK)
+
+        movingsprites.draw(screen)
+        current_room.wall_list.draw(screen)
+
+        pygame.display.flip()
+
+        clock.tick(60)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ pong.py
@@ -0,0 +1,108 @@
+import pygame
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+RESOLUTION_X = 800
+RESOLUTION_Y = 600
+
+
+class Wall(pygame.sprite.Sprite):
+    """This class represents the bar at the bottom that the player controls """
+
+    def __init__(self, x, y, width, height, color):
+        """ Constructor function """
+
+        # Call the parent's constructor
+        super().__init__()
+
+        # Make a BLUE wall, of the size specified in the parameters
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+
+class Player(pygame.sprite.Sprite):
+    """ This class represents the bar at the bottom that the
+    player controls """
+
+    # Set speed vector
+    speed_x = 0
+    speed_y = 0
+
+    def __init__(self, x, y):
+        """ Constructor function """
+
+        # Call the parent's constructor
+        super().__init__()
+
+        # Set height, width
+        self.image = pygame.Surface([100, 10])
+        self.image.fill(WHITE)
+
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
+
+    def changespeed(self, x, y):
+        """ Change the speed of the player. Called with a keypress. """
+        self.speed_x += x
+        self.speed_y += y
+
+    def move(self, walls):
+        """ Find a new position for the player """
+
+        # Move left/right
+        self.rect.x += self.speed_x
+
+        # Did this update cause us to hit a wall?
+        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
+        for block in block_hit_list:
+            # If we are moving right, set our right side to the left side of
+            # the item we hit
+            if self.speed_x > 0:
+                self.rect.right = block.rect.left
+            else:
+                # Otherwise if we are moving left, do the opposite.
+                self.rect.left = block.rect.right
+
+        # Move up/down
+        self.rect.y += self.speed_y
+
+        # Check and see if we hit anything
+        block_hit_list = pygame.sprite.spritecollide(self, walls, False)
+        for block in block_hit_list:
+
+            # Reset our position based on the top/bottom of the object.
+            if self.speed_y > 0:
+                self.rect.bottom = block.rect.top
+            else:
+                self.rect.top = block.rect.bottom
+
+
+class Room(object):
+    """ Base class for all rooms. """
+
+    # Each room has a list of walls, and of enemy sprites.
+    wall_list = pygame.sprite.Group()
+
+    def __init__(self):
+        """ Constructor, create our lists. """
+
+        walls = [
+                 [0, 0, 10, 600, WHITE],
+                 [790, 0, 10, 600, WHITE],
+                 [10, 0, 780, 10, WHITE],
+                 [395, 300, 10, 300, WHITE],
+                 [10, 599, 800, 1, BLACK],
+                 [399, 10, 1, 290, BLACK]
+                ]
+
+        for item in walls:
+            wall = Wall(item[0], item[1], item[2], item[3], item[4])
+            self.wall_list.add(wall)
